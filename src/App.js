088074bb2e0d6c0035b9a2a532282048736cbcca
Micah Bales <micahbales@gmail.com>
2018-09-29 22:27:45 -0400

    Display Pivots for Traffic To and From IP Addresses
    * Handle for clicks on 'to' and 'from' IPs
    * Clicking an IP filters traffic data and updates the view

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import React, { Component } from 'react';
 import trafficData from './data/traffic_bytes';
 import './App.css';
+import {cloneDeep, filter} from 'lodash';
+
+const filterType = {
+  DEFAULT: 0,
+  TO: 1,
+  FROM: 2
+}
 
 const TrafficDataColumns = (props) => {
-  // Add headers to data
-  props.data.unshift({result: {
+  const data = cloneDeep(props.data);
+  // Add headers to traffic data
+  data.unshift({result: {
     'header': 'true',
-    'All_Traffic.dest': 'Traffic Data Source',
-    'All_Traffic.src': 'Traffic Data Destination',
-    'sum_bytes': 'Traffic Data Bytes'
+    'All_Traffic.dest': 'Traffic Data To',
+    'All_Traffic.src': 'Traffic Data From',
+    'sum_bytes': 'Total Bytes Transferred'
   }});
-
   return (
-      props.data.map((rowData, i) => {
+      data.map((rowData, i) => {
         return (
           <div className="row" key={i}>
-            <div className="col-md-4">{rowData.result['All_Traffic.dest']}</div>
-            <div className="col-md-4">{rowData.result['All_Traffic.src']}</div>
-            <div className="col-md-4">{rowData.result['sum_bytes']}</div>
+            <div className={`col-md-4 data-to ${rowData.result.header === 'true' ? '' : 'link-like clickable'}`} 
+                    onClick={props.handleIpClick}>
+                {rowData.result['All_Traffic.dest']}
+            </div>
+            <div className={`col-md-4 data-from ${rowData.result.header === 'true' ? '' : 'link-like clickable'}`} 
+                    onClick={props.handleIpClick}>
+                {rowData.result['All_Traffic.src']}
+            </div>
+            <div className="col-md-4">
+                {rowData.result['sum_bytes']}
+            </div>
           </div>
         );
       })
@@ -27,19 +42,33 @@ const TrafficDataColumns = (props) => {
 const PivotTable = (props) => {
   return (
     <div className="row">
-    <div className="col-md-8 offset-md-2 text-center">
-      <h3>{props.title}</h3>
-    </div>
-    <div className="col-md-8 offset-md-2">
-      <TrafficDataColumns 
-        data={props.trafficData}
-      />
-    </div>
+      <div className="col-md-8 offset-md-2 text-center">
+        <h3>{props.title}</h3>
+      </div>
+      <div className="col-md-8 offset-md-2">
+        <TrafficDataColumns 
+          data={props.data}
+          handleIpClick={props.handleIpClick}
+        />
+      </div>
   </div>
   )
 };
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeIp: null,
+      filter: null,
+      tableTitle: 'All Data Traffic',
+      trafficData: trafficData
+    }
+    // Manually bind React component methods to the class
+    this.handleIpClick = this.handleIpClick.bind(this);
+    this.filterIpData = this.filterIpData.bind(this);
+  }
+
   render() {
     return (
       <div className="App container-fluid">
@@ -51,11 +80,42 @@ class App extends Component {
           </div>
         </div>
         <PivotTable
-          trafficData={trafficData}
-          title="All Data"
+          data={this.state.trafficData}
+          title={this.state.tableTitle}
+          handleIpClick={this.handleIpClick}
         />
       </div>
     );
+  }
+
+  handleIpClick(e) {
+    const ipAddress = e.currentTarget.textContent;
+    const targetClasses = e.currentTarget.className;
+    
+    this.setState({
+      activeIp: ipAddress, 
+      filter: targetClasses.indexOf('data-to') !== -1 ? filterType.TO : filterType.FROM
+    }, () => {
+      // Filter displayed data once state is updated
+      this.filterIpData();
+    });
+  }
+
+  filterIpData() {
+    if (!this.state.activeIp) return;
+
+    const data = cloneDeep(trafficData);
+    const activeIp = this.state.activeIp;
+    const recordKey = this.state.filter !== filterType.FROM ? 'All_Traffic.dest' : 'All_Traffic.src';
+
+    const updatedData = filter(data, (record) => {
+      return record.result[recordKey] === activeIp;
+    });
+
+    this.setState({
+      trafficData: updatedData,
+      tableTitle: `Traffic ${this.state.filter !== filterType.FROM ? 'To' : 'From'} ${activeIp}`
+    });
   }
 }
 
